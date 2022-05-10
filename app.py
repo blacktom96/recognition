22f4f1b77697e8d4758713da7ab1bcbb04c472d9
User <user@yuzanoMacBook-Air.local>
2022-05-10 12:26:44 +0900

    update path

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from sys import path
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request,redirect, url_for,jsonify
 from getData import detectImage, face_distance_to_conf
 from detectDriversLicense import getDriversLicense
 from residenceCard import getResidenceCard
@@ -65,6 +65,99 @@ def check():
         else:
             return render_template("index.html", result="一致しない",accuracy="同じ : " + accuracy,path1 = path1,path2=path2,messages=messages)
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_TYPE= set(['1', '2', '3','4', '5'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/aws/api-recogntion/', methods=["GET", "POST"])
+def recogntion():
+    
+    if 'image1' not in request.files:
+        resp = jsonify({'result':'公的証明書が必要です。'})
+        resp.status_code = 400
+        return resp
+    elif 'image2' not in request.files:
+        resp = jsonify({'result':'顔写真が必要です。'})
+        resp.status_code = 400
+        return resp
+
+
+    image1 = request.files["image1"]
+    image2 = request.files["image2"]
+    messages = ""
+
+    if 'type' not in request.form:
+        resp = jsonify({'result':'公的証明書書類が必要です。'})
+        resp.status_code = 400
+        return resp
+    
+    type = str(request.form["type"])
+    if type not in ALLOWED_TYPE:
+        resp = jsonify({'result':'公的証明書書類が選択されていません。'})
+        resp.status_code = 400
+        return resp
+
+    if image1.filename == '':
+        resp = jsonify({'result':'公的証明書が選択されていません。'})
+        resp.status_code = 400
+        return resp
+    elif image2.filename == '':
+        resp = jsonify({'result':'顔写真が選択されていません。'})
+        resp.status_code = 400
+        return resp
+
+    if (image1 and allowed_file(image1.filename)) and (image2 and allowed_file(image2.filename)):
+        path1 = os.path.join(app.config['UPLOAD_FOLDER'], image1.filename)
+        path2 = os.path.join(app.config['UPLOAD_FOLDER'], image2.filename)
+
+        
+        image1.save(path1)
+        image2.save(path2)
+
+        result_from_detect = detectImage(path1,path2)
+        linear_val = (1.0 - result_from_detect[1]) / (0.8)
+
+        accuracy ='{percent:.2%}'.format(percent=face_distance_to_conf(result_from_detect[1]))
+        # "1":運転免許証
+        # "2":パスポート
+        # "3":保険証 
+        # "4":マイナンバー
+        # "5":在留カード 
+        if(type == "1"): 
+            messages =  getDriversLicense(path1)
+        elif(type == "2"): 
+            messages =  getPassport(path1)
+        elif(type == "3"):
+            messages =  getMyNumber(path1) 
+        elif(type == "4"): 
+            messages =  getResidenceCard(path1) 
+        elif(type == "5"): 
+            messages =  getInsuranceCatd(path1) 
+        if result_from_detect[2]:
+            os.remove(path1)
+            os.remove(path2)
+
+            resp = jsonify({'result':'写真がぼやけてしまいまして、もう一度やり直してください'})
+            resp.status_code = 201
+            return resp
+
+        else:
+            if result_from_detect[0]:
+                resp = jsonify({'result':'一致ます','messages':messages})
+                resp.status_code = 201
+                return resp
+            else:
+                resp = jsonify({'result':'一致しません','messages':messages})
+                resp.status_code = 201
+                return resp
+
+    else:
+        resp = jsonify({'result' : '許可されるファイルタイプは png、jpg、jpegです。'})
+        resp.status_code=400
+        return resp
+	    
 
 @app.route('/<filename>')
 def display_image(filename):
